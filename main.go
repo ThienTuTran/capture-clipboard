b7c3b584a8f6a04407ad600e749757d1e77b5e44
ThienTuTran <jacklogan1912@gmail.com>
2025-10-15 22:03:36 +1000

    feat: store clipboard file in a hidden directory

--- main.go
+++ main.go
@@ -4,21 +4,31 @@ import (
 	"fmt"
 	"log"
 	"time"
+	"os"
+	"path/filepath"
 
 	"github.com/atotto/clipboard"
 )
 
 func main() {
-	log.Println("Clipboard demo started. Press Ctrl+C to stop.")
+	log.Println("Captured Clipboard Demo")
 	var last string
 
 	ticker := time.NewTicker(10 * time.Second)
 	defer ticker.Stop()
 
+	// Determine hidden file path in user's home directory
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatalf("could not determine home directory: %v", err)
+	}
+	hiddenFile := filepath.Join(homeDir, ".clipboard_capture")
+
 	// initial read (non-blocking)
 	if txt, err := clipboard.ReadAll(); err == nil {
 		last = txt
 		fmt.Printf("[%s] initial clipboard: %q\n", time.Now().Format(time.RFC3339), summarize(last))
+		storeClipboard(hiddenFile, last)
 	}
 
 	for range ticker.C {
@@ -31,7 +41,23 @@ func main() {
 		if txt != last {
 			fmt.Printf("[%s] clipboard changed: %q\n", time.Now().Format(time.RFC3339), summarize(txt))
 			last = txt
+			storeClipboard(hiddenFile, txt)
 		}
+	}
+}
+
+// storeClipboard appends clipboard data to a hidden file with restricted permissions
+func storeClipboard(path, data string) {
+	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
+	if err != nil {
+		log.Printf("error: could not open clipboard file: %v", err)
+		return
+	}
+	defer f.Close()
+	timestamp := time.Now().Format(time.RFC3339)
+	entry := fmt.Sprintf("[%s]\n%s\n---\n", timestamp, data)
+	if _, err := f.WriteString(entry); err != nil {
+		log.Printf("error: could not write to clipboard file: %v", err)
 	}
 }
 

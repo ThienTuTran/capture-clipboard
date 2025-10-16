8da8d5c3ec7d5065a928ec7894942943e6f2d30a
ThienTuTran <jacklogan1912@gmail.com>
2025-10-16 20:42:23 +1000

    refactor: Create check function

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"os"
 	"path/filepath"
 	"time"
@@ -18,11 +17,10 @@ func main() {
 
 	// Determine hidden file path in user's home directory
 	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		log.Fatalf("could not determine home directory: %v", err)
-	}
-	hiddenFile := filepath.Join(homeDir, ".clipboard_capture")
+	check(err)
 
+	hiddenFile := filepath.Join(homeDir, ".clipboard_capture")
+	
 	// start clipboard reading loop
 	readClipboard(ticker, hiddenFile)
 }
@@ -32,31 +30,33 @@ func readClipboard(ticker *time.Ticker, hiddenFile string) {
 
 	for {
 		txt, err := clipboard.ReadAll()
-		if err != nil {
-			// If clipboard is locked by another app, skip until next tick
-			log.Printf("warning: could not read clipboard: %v", err)
-		} else if txt != last {
+		check(err)
+
+		if txt != last {
 			fmt.Printf("[%s]: %q\n", time.Now().Format(time.RFC3339), summarize(txt))
 			last = txt
 			if last != "" {
 				storeClipboard(hiddenFile, last)
 			}
 		}
-		<-ticker.C
+		<-ticker.C //wait for next tick
 	}
 }
 
 func storeClipboard(path, data string) {
 	f, err := os.OpenFile(path, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0600)
-	if err != nil {
-		log.Printf("error: could not open clipboard file: %v", err)
-		return
-	}
+	check(err)
+
 	defer f.Close()
 	timestamp := time.Now().Format(time.RFC3339)
 	entry := fmt.Sprintf("[%s]: %s\n", timestamp, data)
-	if _, err := f.WriteString(entry); err != nil {
-		log.Printf("error: could not write to clipboard file: %v", err)
+	_, err = f.WriteString(entry)
+	check(err)
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
 	}
 }
 
